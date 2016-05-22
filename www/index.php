a71ac6462edc80972ba3b0d3014a524e90f60002
literat <tomas@litera.me>
2016-05-22 13:56:54 +0200

    Handling and sending gifts

--- www/index.php
+++ www/index.php
@@ -3,6 +3,42 @@
 use Nette\Application\Routers\Route;
 use Nette\Forms\Form;
 use Nette\Mail\Message;
+use Nette\Utils\Json;
+
+function read_file($file) {
+	$content = @file_get_contents($file);
+	if ($content === FALSE) {
+		throw new Nette\IOException("Unable to read file '$file'.");
+	}
+
+	return $content;
+}
+
+function write_file($file, $content, $mode = 0666) {
+	if (@file_put_contents($file, $content) === FALSE) { // @ is escalated to exception
+		throw new Nette\IOException("Unable to write file '$file'.");
+	}
+	if ($mode !== NULL && !@chmod($file, $mode)) { // @ is escalated to exception
+		throw new Nette\IOException("Unable to chmod file '$file'.");
+	}
+}
+
+function handle_gifts($values, $gifts, $file) {
+	list($item, $id) = explode('-', $values);
+	$id = (int)$id;
+	foreach($gifts->$item as $itm) {
+		if($itm->id == $id) {
+			if($itm->count > 1) {
+				$itm->count--;
+			} else {
+				$itm->reserved = 1;
+			}
+		}
+	}
+
+	$giftsContent = Json::encode($gifts);
+	write_file($file, $giftsContent);
+}
 
 // Load libraries
 require __DIR__ . '/../app/libs/nette.phar';
@@ -20,9 +56,14 @@ $configurator->addConfig(__DIR__ . '/../app/config/config.neon', $configurator::
 $container = $configurator->createContainer();
 $mailer = $container->getService('mailer');
 
+// Read gifts list
+$giftsFile = __DIR__ . '/../app/temp/gifts.json';
+$giftsContent = read_file($giftsFile);
+$gifts = Json::decode($giftsContent);
+
 // Setup routes
 $router = $container->getService('router');
-$router[] = new Route('', function($presenter) use ($mailer) {
+$router[] = new Route('', function($presenter) use ($mailer, $gifts, $giftsFile) {
 
 	// create contact form
 	$form = new Form;
@@ -44,6 +85,8 @@ $router[] = new Route('', function($presenter) use ($mailer) {
 		->setAttribute('class', 'form-control')
 		->setAttribute('placeholder', 'Prosím, něco hezkého mi napište...')
 		->setAttribute('row', 10);
+	$form->addHidden('gifts')
+		->setAttribute('id', 'frm-gifts');
 	$form->addSubmit('send', 'ODESLAT ZPRÁVU')
 		->setAttribute('class', 'btn btn-default btn-xl wow tada');
 
@@ -52,6 +95,12 @@ $router[] = new Route('', function($presenter) use ($mailer) {
 	if(!isset($template->flashMessage)) {
 		$template->flashMessage = '';
 	}
+	if(!isset($template->resetForm)) {
+		$template->resetForm = '';
+	}
+
+	// assign gifts
+	$template->gifts = $gifts;
 	// assign form
 	$template->form = $form;
 
@@ -59,8 +108,13 @@ $router[] = new Route('', function($presenter) use ($mailer) {
 	if ($form->isSuccess()) {
 		$values = $form->getValues();
 
+		if(!empty($values['gifts'])) {
+			handle_gifts($values['gifts'], $gifts, $giftsFile);
+		}
+
 		$message = new Message;
 		$message->addTo('tomas@litera.me')
+			->addTo('dita@litera.me')
 			->setFrom($values['email'], $values['name'])
 			->setSubject($values['subject'])
 			->setBody($values['message']);
@@ -73,7 +127,14 @@ $router[] = new Route('', function($presenter) use ($mailer) {
 		$mailer->send($message);
 
 		$template->flashMessage = 'Vaše zpráva byla odeslána! Děkuji.';
-		$presenter->redirectUrl($presenter->context->httpRequest->url->baseUrl . '#contact-form');
+		$template->resetForm = '<script>
+			document.getElementById("frm-name").value = "";
+			document.getElementById("frm-email").value = "";
+			document.getElementById("frm-subject").value = "";
+			document.getElementById("frm-message").innerText = "";
+			document.getElementById("frm-gifts").value = "";
+		</script>';
+		$presenter->redirectUrl($presenter->context->httpRequest->url->baseUrl . '#contact-form-section');
 	}
 
 	return $template;
